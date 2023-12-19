ce1397c44d563a827e30a7271c79415589a14d41
ayatsakalla <ayat.sakalla@outlook.com>
2023-12-19 18:24:50 -0500

    finished my magical egg!!!

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function shakeMagic8Ball() {
-    const question = prompt('Ask the Magic 8 Ball a Question:');
+    const question = prompt('Ask the Magic Egg a Question:');
 
     if (question === null) {
         document.querySelector('#response-text').innerText = 'Please ask a question!';
-        document.querySelector('#response-text').classList = 'display-4 text-danger';
-        document.querySelector('#response-image').src = '#BAD/BROKENIMG';
+        document.querySelector('#response-text').classList = 'display-4 magic pink';
+        document.querySelector('#response-image').src = 'question.png';
     }
 
     if (!question.trim()) {
@@ -20,56 +20,56 @@ function shakeMagic8Ball() {
         case 0:
         answer = "Yes";
         image = "checkyay.webp";
-        color: "text-success";
+        color = "text-success";
         break;
 
         case 1:
         answer = "No";
-        image = "yes-img.jpg";
-        color: "text-success";
+        image = "x.png";
+        color = "text-danger";
         break;
 
         case 2:
         answer = "Maybe";
-        image = "yes-img.jpg";
-        color: "text-success";
+        image = "shrug.png";
+        color = "text-warning";
         break;
 
         case 3:
         answer = "I don't like you";
-        image = "yes-img.jpg";
-        color: "text-success";
+        image = "ew.png";
+        color = "text-info";
         break;
 
         case 4:
         answer = "Don't talk to me";
-        image = "yes-img.jpg";
-        color: "text-success";
+        image = "talktohand.png";
+        color = "text-danger";
         break;
 
         case 5:
         answer = "Hopeless";
-        image = "yes-img.jpg";
-        color: "text-success";
+        image = "grim.png";
+        color = "text-danger";
         break;
 
         case 6:
         answer = "Possibly yes";
-        image = "yes-img.jpg";
-        color: "text-success";
+        image = "oop.jpg";
+        color = "text-success";
         break;
 
         case 7:
         answer = "Possibly no";
-        image = "yes-img.jpg";
-        color: "text-success";
+        image = "oop.jpg";
+        color = "text-warning";
         break;
     }
 
     const responseText = document.querySelector('#response-text')
-    responseText.innerText = answer
-    responseText.classList = `display-4 ${color}`
-    document.querySelector('response-image').src = image
+    responseText.innerText = answer;
+    responseText.classList = `display-4 magic ${color}`;
+    document.querySelector('#response-image').src = image;
 }
 
 
